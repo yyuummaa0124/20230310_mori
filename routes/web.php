797ae4eead388a72f0f85012e66e8b6d674bc4bc
yyummaa0124 <moriyu01244@gmail.com>
2023-03-21 16:34:02 +0900

    eight

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::group(['middleware' => 'auth'],function() {
     Route::post('/delete', [TodoController::class,'delete'])->name('todo.delete');
     Route::get('/find', [TodoController::class,'find'])->name('todo.find');
     Route::get('/search', [TodoController::class,'search'])->name('todo.search');
+    Route::get('/logout', [TodoController::class,'logout']);
 });
 	
 Route::get('/logout', [TodoController::class,'logout']);
