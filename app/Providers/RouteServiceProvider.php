4bfbc5e2c491deb57ee871e0c2427ce609496714
yyummaa0124 <moriyu01244@gmail.com>
2023-03-21 20:18:13 +0900

    nine

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/todo';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
